ce2170118b71211a8f0a541aec26792c2ac27792
DANIEL BERRETTA <berretta.daniel@gmail.com>
2022-02-07 12:38:07 -0300

    Muestro el Header del index por REACT

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+export function App() {
+    return (
+        <div className="header">
+
+            <div className="navIcon">
+                <a href="./ofrecemos.html">
+                </a>
+            </div>
+
+            <h1 className="titulo">REFORMAS Y REPARACIONES</h1>
+
+            <nav>
+                <div>
+                    <a href="./ofrecemos.html">
+                        <h1>Que ofrecemos?</h1>
+                    </a>
+                </div>
+
+                <div>
+                    <a href="./contacto.html">
+                        <h1>Contacto</h1>
+                    </a>
+                </div>
+
+                <div>
+                    <a href="./login.html">
+                        <h1>Ingrese aqui</h1>
+                    </a>
+                </div>
+            </nav>
+
+        </div>
+    );
+}
