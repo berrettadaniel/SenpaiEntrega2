1305cfda202a9ec771ca0e8d5873a2fb55d2f8c8
DANIEL BERRETTA <berretta.daniel@gmail.com>
2022-02-07 22:29:29 -0300

    Primera version del index con REACT

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,11 @@
+import { Header } from './Header';
+import { IndexGridContainer } from './IndexGridContainer';
+
 export function App() {
     return (
-        <div className="header">
-
-            <div className="navIcon">
-                <a href="./ofrecemos.html">
-                </a>
-            </div>
-
-            <h1 className="titulo">REFORMAS Y REPARACIONES</h1>
-
-            <nav>
-                <div>
-                    <a href="./ofrecemos.html">
-                        <h1>Que ofrecemos?</h1>
-                    </a>
-                </div>
-
-                <div>
-                    <a href="./contacto.html">
-                        <h1>Contacto</h1>
-                    </a>
-                </div>
-
-                <div>
-                    <a href="./login.html">
-                        <h1>Ingrese aqui</h1>
-                    </a>
-                </div>
-            </nav>
-
-        </div>
+        <>
+            <Header />
+            <IndexGridContainer />
+        </>
     );
 }
