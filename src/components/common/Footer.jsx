80a637d7cd3ea1e66c38b112583f3528206a6b5e
DANIEL BERRETTA <berretta.daniel@gmail.com>
2022-02-10 16:12:54 -0300

    Ordenando carpetas del proyecto - paso 2

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,9 @@
+export function Footer() {
+    return (
+        <div class="footerFlex">
+            <h3>Sitio realizado por DB</h3>
+            <h3>e-mail: berretta.daniel@gmail.com</h3>
+            <h3>Celular: (598) 94 054244</h3>
+        </div>
+    );
+}
