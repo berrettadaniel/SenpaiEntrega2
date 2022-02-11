625ea10b93e3abd4a6f9f44f524994aa35f6aba9
DANIEL BERRETTA <berretta.daniel@gmail.com>
2022-02-11 03:49:48 -0300

    HomePage - implementando - revisar img servicios

---
+++ src/components/pages/IndexGridContainer.jsx
@@ -0,0 +1,52 @@
+import { BannerSitio } from "../common/BannerSitio.jsx";
+import { Footer } from "../common/Footer.jsx";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export function IndexGridContainer() {
+    //Para el manejo de estados
+    const [servicios, setServicios] = useState([]);
+
+    //Efecto secundario
+    useEffect(() =>{
+        axios.get('http://localhost:4000/servicios').then((response) => {
+            setServicios(response.data);
+        });
+    }, []);
+
+
+
+    return (
+        <div class="gridContainer">
+        
+            {/* PRIMERA FILA DE GRILLA
+                Sponsors y slogan del sitio */}
+            <div class="advertisement1"></div>
+            <BannerSitio />
+            <div class="advertisement2"></div>
+
+            {/* SEGUNDA FILA DE GRILLA
+                Lista de servicios que se ofrecen */}
+            <div class="servicesFlex">
+
+                {servicios.map(function(servicio) {
+                    return (
+                        <div class="service">
+                            <a href="#" target="_blank">
+                                <img
+                                    src={servicio.archivo}
+                                    alt={servicio.nombre} />
+                                <h2>Albañilería</h2>
+                            </a>
+                        </div>
+                    )})}
+
+
+            </div>
+
+            {/* TERCERA FILA DE GRILLA
+                Pie de la pagina */}
+            <Footer />
+        </div>
+    );
+}
---
+++ src/components/pages/IndexHomePage.jsx
@@ -0,0 +1,13 @@
+import { Header } from "../common/Header";
+import { IndexGridContainer } from "./IndexGridContainer";
+
+export function IndexHomePage() {
+    return (
+        <body>
+            <Header titulo = "REFORMAS Y REPARACIONES" />
+
+            <IndexGridContainer />
+
+        </body>
+);
+}
