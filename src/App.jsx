c75bd42923bacd19fe0fd96f650976fd3786326a
DANIEL BERRETTA <berretta.daniel@gmail.com>
2022-02-12 20:32:45 -0300

    LoginPage - con ERRORES

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes, Link } from 'react-router-dom';
 import { IndexPage } from './components/pages/IndexPage';
 import { OfrecemosPage } from './components/pages/OfrecemosPage';
+import { LoginPage } from './components/pages/LoginPage';
 
 export function App() {
     return (
         <Routes>
             <Route path="/" element={<IndexPage />} />
-            <Route path="ofrecemos" element={<OfrecemosPage />} />   
+            <Route path="ofrecemos" element={<OfrecemosPage />} />
+            <Route path="login" element={<LoginPage />} />
         </Routes>
     );
 }
