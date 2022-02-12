11514c4468ea4cd77388ecf8ce45c9e8a878b24c
DANIEL BERRETTA <berretta.daniel@gmail.com>
2022-02-12 11:59:43 -0300

    Header parametrico

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,11 +1,57 @@
 import '../../assets/styles/style.css';
 
+function navOp1(props) {
+    const op1 = props.opcion1;
+    const op2 = props.opcion2;
+    const op3 = props.opcion3;
+
+    if (op1 !== "" || op2 !== "" || op3 !== "" ) {
+        return (
+            <nav>
+                <div>
+                    <a><h2>{op1}</h2></a>
+                {/*    <a href="./ofrecemos.html">
+                        <h2>Que ofrecemos?</h2>
+                    </a> */}
+                </div>
+
+                <div>
+                    <a><h2>{op1}</h2></a>
+                {/*    <a href="./contacto.html">
+                        <h2>Contacto</h2>
+                    </a> */}
+                </div>
+
+                <div>
+                    <a><h2>{op3}</h2></a>
+                {/*    <a href="./login.html">
+                        <h2>Ingrese aqui</h2>
+                    </a> */}
+                </div>
+            </nav>
+        )
+    } else {
+        return (
+            <nav>
+                <div>
+                    <a>"Volver"</a>
+                {/*    <a href="./ofrecemos.html">
+                        <h2>Que ofrecemos?</h2>
+                    </a> */}
+                </div>
+            </nav>
+        )
+    }
+}
+
 export function Header(props) {
     return (
         <div className="header">
 
             <h1 className="titulo">{ props.titulo }</h1>
 
+            <navOp1 />
+{/*
             <nav>
                 <div>
                     <a href="./ofrecemos.html">
@@ -25,7 +71,7 @@ export function Header(props) {
                     </a>
                 </div>
             </nav>
-
+*/}
         </div>
     );
 }
--- src/components/pages/IndexPage.jsx
+++ src/components/pages/IndexPage.jsx
@@ -5,7 +5,10 @@ import { IndexGridContainer } from "./IndexGridContainer";
 export function IndexPage() {
     return (
         <body>
-            <Header titulo = "REFORMAS Y REPARACIONES" />
+            <Header titulo = "REFORMAS Y REPARACIONES"
+                    opcion1 = "Que ofrecemos?"
+                    opcion2 = "Contacto"
+                    opcion3 = "Ingrese aqui" />
             <IndexGridContainer />
         </body>
 );
