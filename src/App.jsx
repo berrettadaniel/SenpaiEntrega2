607e9130b311908e00cc33832ad0344bb3331ec4
DANIEL BERRETTA <berretta.daniel@gmail.com>
2022-02-12 21:43:37 -0300

    ContactoPage

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes, Link } from 'react-router-dom';
 import { IndexPage } from './components/pages/IndexPage';
 import { OfrecemosPage } from './components/pages/OfrecemosPage';
 import { LoginPage } from './components/pages/LoginPage';
+import { ContactoPage } from './components/pages/ContactoPage';
 
 export function App() {
     return (
@@ -9,6 +10,7 @@ export function App() {
             <Route path="/" element={<IndexPage />} />
             <Route path="ofrecemos" element={<OfrecemosPage />} />
             <Route path="login" element={<LoginPage />} />
+            <Route path="contacto" element={<ContactoPage />} />
         </Routes>
     );
 }
